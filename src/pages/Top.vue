9cd307dc649e7d50582dd4af90b4129e4dc728ee
Bwambocos <babcs2035@gmail.com>
2022-12-16 23:44:00 +0900

    [update] dynamic size

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,15 +1,19 @@
 <template>
 	<main>
 		<div class="wrapper">
-			<div v-for="i in rows" :key="i" class="row">
-				<div v-for="j in columns" :key="j" @click="set(i, j)">
+			<div v-for="i in size" :key="i" class="row">
+				<div v-for="j in size" :key="j" @click="set(i, j)">
 					<div v-if="check(i, j)" class="box fill"></div>
 					<div v-else class="box"></div>
 				</div>
 			</div>
+			<div class="buttons">
+				<button type="button" @click="add()">+</button>
+				<button type="button" @click="sub()">-</button>
+			</div>
 			<button type="button" @click="reset()">リセット</button>
 			<div class="output">
-				<textarea cols="35" rows="16" v-model="output"></textarea>
+				<textarea :cols="size * 2 + 2" :rows="size" v-model="output"></textarea>
 			</div>
 		</div>
 	</main>
@@ -19,22 +23,29 @@
 export default {
 	data() {
 		return {
-			rows: 16,
-			columns: 16,
+			size: 8,
 			field: [[]],
 		};
 	},
 
 	created() {
-		this.field = Array(this.rows);
-		for (let i = 0; i < this.rows; ++i) {
-			this.field[i] = Array(this.columns);
-			this.field[i].fill(0);
-		}
+		let flag = false;
 		if (window.localStorage) {
+			this.size = Number(localStorage.getItem("size"));
 			let json = localStorage.getItem("field");
 			if (json != undefined) {
-				this.field = JSON.parse(json);
+				const array = JSON.parse(json);
+				if (array.length == this.size) {
+					this.field = array;
+					flag = true;
+				}
+			}
+		}
+		if (!flag) {
+			this.field = Array(this.size);
+			for (let i = 0; i < this.size; ++i) {
+				this.field[i] = Array(this.size);
+				this.field[i].fill(0);
 			}
 		}
 	},
@@ -53,13 +64,26 @@ export default {
 			if (window.localStorage) {
 				let json = JSON.stringify(this.field, undefined, 1);
 				localStorage.setItem("field", json);
+				localStorage.setItem("size", this.size.toString());
+			}
+		},
+
+		add() {
+			this.size++;
+			this.reset();
+		},
+
+		sub() {
+			if (this.size > 1) {
+				this.size--;
+				this.reset();
 			}
 		},
 
 		reset() {
-			this.field = Array(this.rows);
-			for (let i = 0; i < this.rows; ++i) {
-				this.field[i] = Array(this.columns);
+			this.field = Array(this.size);
+			for (let i = 0; i < this.size; ++i) {
+				this.field[i] = Array(this.size);
 				this.field[i].fill(0);
 			}
 		},
@@ -68,9 +92,9 @@ export default {
 	computed: {
 		output() {
 			let res = "[";
-			for (let i = 0; i < this.rows; ++i) {
+			for (let i = 0; i < this.size; ++i) {
 				res += "[" + this.field[i].join(",") + "]";
-				if (i < this.rows - 1) {
+				if (i < this.size - 1) {
 					res += ",";
 				}
 			}
@@ -105,6 +129,6 @@ export default {
 }
 
 button {
-	margin: 2vh 0;
+	margin: 1.5vh 1vw;
 }
 </style>
